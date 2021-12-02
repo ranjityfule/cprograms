69c2152f38a189e4004751c218f186cc29467999
ranjityfule <ranjityfule@gmail.com>
2021-12-02 14:35:26 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@ echo "Running tests..."
 echo
 
 output=$(./a.out)
-expected_output="Hello, World!"
+expected_output="[10, 20, 30, 40, 50]"
 
 if [ $? -eq 0 ] ; then
   echo "Pass: Program exited zero"
